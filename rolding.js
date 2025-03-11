264d7422dfef69db2b53106056fc994ab716c527
unknown <miguelsellaprado10@gmail.com>
2025-03-11 15:27:55 -0300

    nome alterado <new-branch> nome novo <boasVindas>.

---
+++ rolding.js
@@ -0,0 +1,5 @@
+let nomeUsuario = prompt("insira seu nome.");
+
+console.log(`ola, espero que você esteja tendo um ótimo dia ${nomeUsuario}.`)
+
+//nome alterado nome atingo--> <new-branch>  novo nome--> <boasVindas>
